9bec3cbb512fb3ac1d8f14c97b8f31ffc08c1629
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-09-30 17:44:40 -0400

    Basic player

---
+++ game/player.lua
@@ -0,0 +1,14 @@
+local direction = 2
+
+function look_up(e, instance)
+end
+
+function look_right(e, instance)
+end
+
+function look_down(e, instance)
+end
+
+function look_left(e, instance)
+end
+
---
+++ game/room.lua
@@ -0,0 +1,3 @@
+function create_room(e, instance)
+	action(e, instance, '"type": "window", "w": 768, "h": 768')
+end
