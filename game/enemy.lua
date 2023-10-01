97cae934b09cbde13f4953493decc9cf8ed8cff9
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-10-01 18:04:19 -0400

    Final touches

--- game/enemy.lua
+++ game/enemy.lua
@@ -1,5 +1,5 @@
 function enemy_bullet(e, instance)
-	local rand = math.random(0,3) -- 25% chance
+	local rand = math.random(0,1) -- 50% chance
 	if (rand == 0)
 	then
 		local angle = math.random(0, 359)*(math.pi/180)
@@ -8,5 +8,14 @@ function enemy_bullet(e, instance)
 		local h = speed*math.sin(angle)
 		local v = speed*math.cos(angle)
 		action(e, bullet, '"type": "speed", "h": ' .. h .. ', "v": ' .. v)
+	end
+end
+
+function multiply(e, instance)
+	local rand = math.random(0,1)
+	if (rand == 0)
+	then
+		spawn(e, instance, "enemy.json", 1, 0, true)
+		spawn(e, instance, "enemy.json", 0, 1, true)
 	end
 end
