6e0f71cfd40d0847582039f41f82c2439b075aac
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-10-01 11:07:36 -0400

    Better player

--- game/player.lua
+++ game/player.lua
@@ -1,14 +1,88 @@
-local direction = 2
+local move_direction = 2 --0 up, 1 right, 2 down, 3 left
+local shoot_direction = 2
+local can_shoot = true
 
-function look_up(e, instance)
+local function set_frame()
+	print(move_direction .. " " .. shoot_direction)
 end
 
-function look_right(e, instance)
+function player_timer(e, instance)
+	can_shoot = true
 end
 
-function look_down(e, instance)
+function move_up(e, instance)
+	move_direction = 0
+	set_frame()
 end
 
-function look_left(e, instance)
+function move_right(e, instance)
+	move_direction = 1
+	set_frame()
 end
+
+function move_down(e, instance)
+	move_direction = 2
+	set_frame()
+end
+
+function move_left(e, instance)
+	move_direction = 3
+	set_frame()
+end
+
+function shoot_up(e, instance)
+	if (can_shoot == true)
+	then
+		local bullet = spawn(e, instance, "bullet_up.json", 0, -25, true)
+		can_shoot = false
+		action(e, instance, '"type": "timer", "num": 2, "time": 0.25')
+	end
+	shoot_direction = 0
+	set_frame()
+end
+
+function shoot_right(e, instance)
+	if (can_shoot == true)
+	then
+		local bullet = spawn(e, instance, "bullet_right.json", 25, 0, true)
+		can_shoot = false
+		action(e, instance, '"type": "timer", "num": 2, "time": 0.25')
+	end
+	shoot_direction = 1
+	set_frame()
+end
+
+function shoot_down(e, instance)
+	if (can_shoot == true)
+	then
+		local bullet = spawn(e, instance, "bullet_down.json", 0, 25, true)
+		can_shoot = false
+		action(e, instance, '"type": "timer", "num": 2, "time": 0.25')
+	end
+	shoot_direction = 2
+	set_frame()
+end
+
+function shoot_left(e, instance)
+	if (can_shoot == true)
+	then
+		local bullet = spawn(e, instance, "bullet_left.json", -25, 0, true)
+		can_shoot = false
+		action(e, instance, '"type": "timer", "num": 2, "time": 0.25')
+	end
+	shoot_direction = 3
+	set_frame()
+end
+
+--[[function release_up(e, instance)
+end
+
+function release_right(e, instance)
+end
+
+function release_down(e, instance)
+end
+
+function release_left(e, instance)
+end]]--
 
